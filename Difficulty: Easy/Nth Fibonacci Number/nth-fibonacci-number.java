99fc8d2e669f1f5e32ab37134c16a142f4d15009
swetalj <128052552+swetalj@users.noreply.github.com>
2025-02-06 21:43:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.nthFibonacci(n));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int nthFibonacci(int n) {
+        // code here
+        if(n == 0) 
+          return 0;
+        if(n == 1)
+           return 1;
+        if(n>1)
+           return nthFibonacci(n-1) + nthFibonacci(n-2);
+      return -1;
+    }
+}

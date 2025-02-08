429fcf63ebd3c10551fec23bb542967300e5203a
swetalj <128052552+swetalj@users.noreply.github.com>
2025-02-09 01:31:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count ways to N'th Stair/count-ways-to-nth-stair.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            // taking total number of stairs
+            int n = sc.nextInt();
+
+            // creating an object of class DynamicProgramming
+            Solution obj = new Solution();
+
+            // calling method of class countWays()
+            // of class DynamicProgramming
+            System.out.println(obj.countWays(n));
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    Long countWays(int n) {
+        // your code here
+         long count=1;
+        for(int i=1;i<=n;i++){
+            if((n-2*i)>=0 && (2*i+(n-2*i)*1==n)){
+                count++;
+            }
+        }
+        return count;
+    }
+}

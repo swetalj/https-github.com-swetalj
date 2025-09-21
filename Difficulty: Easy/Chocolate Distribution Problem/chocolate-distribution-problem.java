1abdb6f9da3004e47ab269861a3b3a7801afee58
swetalj <128052552+swetalj@users.noreply.github.com>
2025-09-22 00:43:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findMinDiff(ArrayList<Integer> a, int m) {
+        int n = a.size();
+        Collections.sort(a);
+
+        int start = 0, end = 0;
+        // Largest number of chocolates
+        int mind = Integer.MAX_VALUE;
+
+        // Find the subarray of size m such that
+        // difference between last (maximum in case
+        // of sorted) and first (minimum in case of
+        // sorted) elements of subarray is minimum.
+        for (int i = 0; i + m - 1 < n; i++) {
+            int diff = a.get(i + m - 1) - a.get(i);
+            if (mind > diff) {
+                mind = diff;
+                start = i;
+                end = i + m - 1;
+            }
+        }
+        return a.get(end) - a.get(start);
+    }
+}
